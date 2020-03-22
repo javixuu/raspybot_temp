0fa758cf2e01da1e3922e4e518fc0aac9dd92c78
javixuu <31996659+javixuu@users.noreply.github.com>
2020-03-22 18:10:42 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+from mq2 import *
 import sys
 import time
 import random
@@ -6,15 +7,17 @@ import telepot
 import Adafruit_DHT
 import requests
 
+
 def handle(msg):
 	chat_id = msg['chat']['id']
 	command = msg['text']
 
 	print 'Got command : %s' % command
-        humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,4)
 	if command == '/hey':
-		bot.sendMessage(chat_id, 'hellooou my friend')
+		perc = mq.MQPercentage()
+		bot.sendMessage(chat_id, "LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
         elif command == '/temp':
+		humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,4)
                 bot.sendMessage(chat_id, 'temp = %.4f' % temp)
         else:
 		bot.sendMessage(chat_id, 'sorry mate, i am stupid')
@@ -32,6 +35,8 @@ def notify():
 bot = telepot.Bot('1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg')
 bot.message_loop(handle)
 print 'I am listening...'
+
+mq = MQ();
 
 while 1:
     print 'here...'
