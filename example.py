93f4b6596f70e939d297c68dc0c2a4486878850c
Javi <jmaker3d@gmail.com>
2020-03-22 17:44:10 +0000

    update2

--- example.py
+++ example.py
@@ -14,10 +14,8 @@ def handle(msg):
 
 	print ('Got command : %s' % command)
 	if command == '/hey':
-		perc = mq.MQPercentage()
 		bot.sendMessage(chat_id, "LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
 	elif command == '/temp':
-		humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,4)
 		bot.sendMessage(chat_id, 'temp = %.4f' % temp)
 	else:
 		bot.sendMessage(chat_id, 'sorry mate, i am stupid')
@@ -25,22 +23,23 @@ def handle(msg):
 def notify():
         bot_token='1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg'
         bot_chatID='401663194'
-        temp = 24
-        if temp > 35 : 
+        if temp > 35:
             send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'higt temp!!!'
             response=requests.get(send_text)
             print(response)
-            
-            
+
+
 bot = telepot.Bot('1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg')
 bot.message_loop(handle)
-print ('I am listening...')
-
+print ('Started, sensors ready in 30 sec')
 mq = MQ();
-
+global temp
+global perc
 while 1:
-    print ('here...')
-    notify()
-    time.sleep(10)
+	print ('here...')
+	perc = mq.MQPercentage()
+	humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,4)
+	notify()
+	time.sleep(10)
 
 
