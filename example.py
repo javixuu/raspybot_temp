ca0488e560ba53d19954f65884e8d98e47d327b1
Javi <jmaker3d@gmail.com>
2020-03-22 18:01:26 +0000

    small changes

--- example.py
+++ example.py
@@ -21,20 +21,20 @@ def handle(msg):
 		bot.sendMessage(chat_id, 'sorry mate, i am stupid')
 
 def notify():
-        bot_token='1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg'
-        bot_chatID='401663194'
-        if temp > 35:
-            send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'higt temp!!!'
-            response=requests.get(send_text)
-            print(response)
-	 if perc["CO"]>1:
-            send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'CO > 1!!!'
-            response=requests.get(send_text)
-            print(response)
+	bot_token='1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg'
+	bot_chatID='401663194'
+	if temp > 35:
+		send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'higt temp!!!'
+		response=requests.get(send_text)
+		print(response)
+	if perc["CO"] > 1:
+		send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'CO > 1!!!'
+		response=requests.get(send_text)
+		print(response)
 	if perc["SMOKE"]>1:
-            send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'Posible fuego!!!'
-            response=requests.get(send_text)
-            print(response)
+		send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'Posible fuego!!!'
+		response=requests.get(send_text)
+		print(response)
 
 
 bot = telepot.Bot('1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg')
