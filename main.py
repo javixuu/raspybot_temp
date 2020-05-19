f8aadaa2e485031251e1d57222021dac534f0f18
javixuu <31996659+javixuu@users.noreply.github.com>
2020-05-19 11:00:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def handle(msg):
 		bot.sendMessage(chat_id, 'sorry mate, i am stupid')
 
 def notify():
-	bot_token='1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg'
-	bot_chatID='401663194'
+	bot_token='<bot token>'
+	bot_chatID='<chat id>'
 	if temp > 35:
 		send_text='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + 'higt temp!!!'
 		response=requests.get(send_text)
@@ -36,7 +36,7 @@ def notify():
 		print(response)
 
 
-bot = telepot.Bot('1012793011:AAE-Y5p6Q6FfJWQ_6XGsVzjrLNDE64F6qcg')
+bot = telepot.Bot('<bot token>')
 bot.message_loop(handle)
 bot_token='<bot token>'
 bot_chatID='<chat id>'
